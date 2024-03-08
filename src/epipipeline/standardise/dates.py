0543446a5359d579fc8e9d5bdbdb0fcd2f549b89
Sai Sneha <sneha@artpark.in>
2024-03-08 15:14:38 +0530

    fix: removing redundancies that were causing dates to be set to NaN

--- src/epipipeline/standardise/dates.py
+++ src/epipipeline/standardise/dates.py
@@ -23,16 +23,13 @@ def validate_dates(df, year_of_data, date_columns):
 
     # Function to handle various date formats and coerce invalid formats to NaT
     def parse_date_v2(date):
-        try:
-            if re.match(r'^\d{5}$', str(date)):
-                date = pd.to_datetime(int(date), unit='D', origin='1899-12-30')
-            else:
-                date = pd.to_datetime(date, errors='coerce', dayfirst=True)
-            if date.year not in [year_of_data, year_of_data - 1, year_of_data + 1] or date > datetime.now():
-                return pd.NaT
-            return date
-        except ParserError:
+        if re.match(r'^\d{5}$', str(date)):
+            date = pd.to_datetime(int(date), unit='D', origin='1899-12-30')
+        else:
+            date = pd.to_datetime(date, errors="coerce", dayfirst=True)
+        if date.year not in [year_of_data, year_of_data - 1, year_of_data + 1] or date > datetime.now():
             return pd.NaT
+        return date
 
     # Function to validate the order of dates
     # def validate_order(row):

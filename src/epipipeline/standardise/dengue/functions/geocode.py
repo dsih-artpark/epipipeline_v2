bb56a8c5c73e386ce7d40dafd980185d44211e88
anujavenkatachalam04 <anuja.venkatachalam@ifmr.ac.in>
2024-03-20 13:29:21 +0530

    update: modified geocode function

--- src/epipipeline/standardise/dengue/functions/geocode.py
+++ src/epipipeline/standardise/dengue/functions/geocode.py
@@ -1,14 +1,18 @@
-# Dependencies - dotenv
-# Save 
 from dotenv import load_dotenv
 import os
+import pandas as pd
+from googlemaps import Client as GoogleMaps
+import googlemaps
+import gmaps
 
 load_dotenv()
 
-API=os.getenv('GMAPS_API')
+try:
+    MyAPI=os.getenv("GMAPS_API")
+except:
+    raise (".env var not found. Store your GMAPS API in a .env file with varname GMAPS_API")
 
-
-def geocode(full_address: str, API_Key) -> tuple:
+def geocode(full_address: str, MyAPI: str) -> tuple:
     """_summary_
 
     Args:
@@ -17,11 +21,8 @@ def geocode(full_address: str, API_Key) -> tuple:
     Returns:
         tuple: lat, long
     """
-    import pandas as pd
-    from googlemaps import Client as GoogleMaps
-    import googlemaps
-    import gmaps
-
+    assert isinstance(full_address, str) and isinstance(MyAPI,str) and len(MyAPI)==39, "invalid input"
+    gmaps = googlemaps.Client(key=MyAPI)
     geocode_result = gmaps.geocode(full_address)
     lat= geocode_result[0]['geometry']['location'] ['lat']
     long= geocode_result[0]['geometry']['location']['lng']

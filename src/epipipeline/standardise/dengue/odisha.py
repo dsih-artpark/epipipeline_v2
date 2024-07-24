e9fc5be810c9731f0d71539e2890559f2a8c09bc
anujavenkatachalam04 <anuja.venkatachalam@ifmr.ac.in>
2024-07-24 12:24:19 +0530

    updated gender, death functions in odisha.py

--- src/epipipeline/standardise/dengue/odisha.py
+++ src/epipipeline/standardise/dengue/odisha.py
@@ -4,7 +4,7 @@ import re
 import dataio.download
 from dataio.download import (download_dataset_v2,fetch_data_documentation)
 from epipipeline.standardise.gis import (dist_mapping, subdist_ulb_mapping, village_ward_mapping)
-from epipipeline.standardise import (validate_age, standardise_age, standardise_gender, standardise_test_result, clean_strings, rural_urban)
+from epipipeline.standardise import ( extract_gender_age, standardise_age, standardise_gender, validate_age, standardise_test_result, clean_strings, rural_urban, event_death)
 from epipipeline.standardise.dates import (string_clean_dates)
 import yaml
 import os
@@ -22,19 +22,13 @@ str_cols = metadata["admin"]["config"]["str_cols"]
 regions=pd.read_csv("regions/regionids.csv")
 
 # download preprocessed files
-pp_csvs = download_dataset_v2(dsid=rdsid, data_state = "preprocessed")
-
-# Consolidate
-# main_df=pd.DataFrame()
-
-# for file in os.listdir(f"orissa/preprocessed"):
-#     df=pd.read_csv(f"orissa/preprocessed/{file}")
-#     df["file"]=file
-#     main_df=main_df._append(df)
-
-# main_df.to_csv("odisha-std-ll.csv", index=False)
+pp_csvs = download_dataset_v2(dsid=dsid, data_state = "preprocessed")
 
 # Standardisation
+for file in os.listdir(f"data/{pp_csvs}"):
+    df=pd.read_csv(f"data/{pp_csvs}{file}")
+
+
 df=pd.read_csv("odisha-std-ll.csv")
 
 for col in data_dictionary.keys():
@@ -50,32 +44,41 @@ df["event.test.resultDate"]=df["event.test.resultDate"].apply(lambda x: string_c
 df["event.test.resultDate"]=df["event.test.resultDate"].dt.strftime('%Y-%m-%dT%H:%M:%SZ')
 df["metadata.primaryDate"]=df["event.test.resultDate"]
 
-# Standardise Age - to combine with gender - no separator
-# df["demographics.age"] = df["demographics.age"].apply(lambda x: standardise_age(age=x))
+# Extract gender, age - values are swapped
+res = df.apply(lambda x: extract_gender_age(gender=x["demographics.gender"], age=x["demographics.age"]), axis=1)
+df["demographics.gender"], df["demographics.age"] = zip(*res)
+
+# Standardise Age
+df["demographics.age"] = df["demographics.age"].apply(lambda x: standardise_age(age=x))
 
 # # Validate Age - 0 to 105
-# df["demographics.age"] = df["demographics.age"].apply(lambda x: validate_age(age=x))
+df["demographics.age"] = df["demographics.age"].apply(lambda x: validate_age(age=x))
 
 # Bin Age
-# df["demographics.ageRange"] = pd.cut(df["demographics.age"].fillna(-999), bins=[0, 1, 6, 12, 18, 25, 45, 65, 105], include_lowest=False)
-# df.loc[df["demographics.age"].isna(), "demographics.ageRange"] = pd.NA
+df["demographics.ageRange"] = pd.cut(df["demographics.age"].fillna(-999), bins=[0, 1, 6, 12, 18, 25, 45, 65, 105], include_lowest=False)
+df.loc[df["demographics.age"].isna(), "demographics.ageRange"] = pd.NA
 
-#Standardise Gender - MALE, FEMALE, UNKNOWN
-# df["demographics.gender"] = df["demographics.gender"].apply(lambda x: standardise_gender(gender=x))
+# Standardise Gender
+df["demographics.gender"] = df["demographics.gender"].apply(lambda x: standardise_gender(gender=x))
 
 # Standardise Result variables - POSITIVE, NEGATIVE, UNKNOWN
 df["event.test.test1.result"] = df["event.test.test1.result"].apply(lambda x: standardise_test_result(result=x))
 df["event.test.test2.result"] = df["event.test.test2.result"].apply(lambda x: standardise_test_result(result=x))
 
-# Standardise case variables
-# OPD, IPD
+# Standardise Case variables
 df["case.urbanOrRural"] = df["case.urbanOrRural"].apply(lambda x: rural_urban(s=x))
+
+# Standardise event.death
+df["event.death"] = df["event.death"].apply(lambda x: event_death(s=x))
 
 # Clean string vars
 for var in str_cols:
     if var in df.columns:
         df[var] = df[var].apply(lambda x: clean_strings(s=x))
 
+# Extract mobile from address
+
+## GEOGRAPHY
 # districts
 res=df.apply(lambda x: dist_mapping(stateID=x["location.admin1.ID"], districtName=x["location.admin2.name"], df=regions), axis=1)
 df["location.admin2.name"], df["location.admin2.ID"]=zip(*res)
@@ -91,8 +94,7 @@ df["location.admin5.name"], df["location.admin5.ID"]= zip(*res)
 # admin hierarchy
 df["location.admin.hierarchy"] = df["location.admin3.ID"].apply(lambda x: "ULB" if x.startswith("ulb") else ("Revenue" if x.startswith("subdistrict") else "admin_0"))  # noqa: E501
 
-
-# Generate admin coarseness
+# Generate admin coarseness - rewrite function
 L=["location.admin2.ID", "location.admin3.ID", "location.admin5.ID"]
 
 for vars in L:
@@ -119,7 +121,5 @@ df["metadata.recordID"]=[uuid.uuid4() for _ in range(len(df))]
 df=df[data_dictionary.keys()]
 
 # export current file
-df.to_csv("odisha-std-ll.csv", index=False)
-
-
-
+df.to_csv("odisha-std-ll-new.csv", index=False)
+#------
